29bbc2f9e73501a6beb4e71001d1d7de0ba53f49
cps97 <colinstoutt@gmail.com>
2023-03-07 15:43:55 -0800

    create recipe model

---
+++ src/models/Recipe.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+type ingredient = {
+  name: string;
+  cal: number;
+};
+const ingredientSchema = new mongoose.Schema<ingredient>({
+  name: { type: String, required: true },
+  cal: { type: Number },
+});
+
+type instruction = {
+  step: string;
+};
+const instructionSchema = new mongoose.Schema<instruction>({
+  step: { type: String, required: true },
+});
+
+type recipe = {
+  name: string;
+  imageUrl: string;
+  prepTime: number;
+  cookTime: number;
+  ingedients: ingredient[];
+  instructions: instruction[];
+};
+const recipeSchema = new mongoose.Schema<recipe>({
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  prepTime: {
+    type: Number,
+    required: true,
+  },
+  cookTime: {
+    type: Number,
+    required: true,
+  },
+  ingedients: {
+    type: [ingredientSchema],
+    required: true,
+  },
+  instructions: {
+    type: [instructionSchema],
+    required: true,
+  },
+});
+
+export default mongoose.models.Recipe ||
+  mongoose.model<recipe>("Recipe", recipeSchema);
