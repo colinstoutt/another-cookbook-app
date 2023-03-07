d59dd98ea6ea0a1cde16ea68ac49a8414fe553ff
cps97 <colinstoutt@gmail.com>
2023-03-07 15:26:32 -0800

    remove disconnect funtion

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -7,18 +7,15 @@ export async function dbConnect() {
   if (db) {
     return;
   }
+  // conenct to mongoDb
   await Mongoose.connect(process.env.MONGO_URI!);
 
   db = Mongoose.connection;
+
+  // console msgs and errs
   db.on("open", () =>
     console.log(`Connected to MongoDB on ${db.host}:${db.port}`)
   )
     .on("close", () => console.log("Connection Closed"))
     .on("error", (error) => console.log(error));
 }
-export const disconnect = () => {
-  if (!db) {
-    return;
-  }
-  Mongoose.disconnect();
-};
