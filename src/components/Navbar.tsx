ee30f0193c41237b4a5250d532ddfb60636421bd
cps97 <colinstoutt@gmail.com>
2023-03-07 22:39:48 -0800

    build and style navbar. add menu toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,62 @@
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import RestaurantMenuRoundedIcon from "@mui/icons-material/RestaurantMenuRounded";
+import ClearRoundedIcon from "@mui/icons-material/ClearRounded";
+
+import Link from "next/link";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  function handleToggleMenu() {
+    setToggleMenu(!toggleMenu);
+  }
+
+  const Menu = () => {
+    return (
+      <ul className="nav__menu">
+        <li>
+          <Link className="nav__menu-link" href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav__menu-link" href="/">
+            Create Recipe
+          </Link>
+        </li>
+        <li>
+          <Link className="nav__menu-link" href="/">
+            Discover
+          </Link>
+        </li>
+      </ul>
+    );
+  };
+
   return (
-    <nav className="nav">
-      <RestaurantMenuRoundedIcon
-        style={{ fontSize: "2.5rem", color: "white" }}
-      />
-      <MenuRoundedIcon style={{ fontSize: "2.5rem", color: "white" }} />
-    </nav>
+    <>
+      <nav className="nav">
+        <RestaurantMenuRoundedIcon
+          style={{ fontSize: "2.5rem", color: "white" }}
+          className="nav__icon"
+        />
+        {!toggleMenu ? (
+          <MenuRoundedIcon
+            onClick={handleToggleMenu}
+            style={{ fontSize: "2.5rem", color: "white" }}
+            className="nav__icon"
+          />
+        ) : (
+          <ClearRoundedIcon
+            onClick={handleToggleMenu}
+            style={{ fontSize: "2.5rem", color: "white" }}
+            className="nav__icon"
+          />
+        )}
+      </nav>
+      {toggleMenu ? <Menu /> : <></>}
+    </>
   );
 };
 
