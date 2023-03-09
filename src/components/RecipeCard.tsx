ffd18d646e56d7ec6326a43e2621fc5485bc0133
cps97 <colinstoutt@gmail.com>
2023-03-08 23:57:40 -0800

    give recipeCard its own component

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Image from "next/image";
+import Instruction from "@/types/instruction";
+import Ingredient from "@/types/ingredient";
 
-const RecipeCard = () => {
-  return <div>RecipeCard</div>;
+interface Recipe {
+  name: string;
+  imageUrl: string;
+  prepTime: number;
+  cookTime: number;
+  ingredients: Ingredient[];
+  instructions: Instruction[];
+  _id: number;
+}
+const RecipeCard = ({ recipe }: Recipe) => {
+  return (
+    <>
+      <div className="index__recipe-card" key={recipe._id}>
+        <h1 className="index__recipe-card-title">{recipe.name}</h1>
+        <Image
+          src={recipe.imageUrl}
+          alt={recipe.name}
+          style={{ objectFit: "cover", borderRadius: "10px" }}
+          width={400}
+          height={200}
+          priority
+        />
+      </div>
+    </>
+  );
 };
 
 export default RecipeCard;
