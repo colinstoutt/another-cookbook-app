80045690b6d6dd248c5bdc52ed00bcf8d37487b9
cps97 <colinstoutt@gmail.com>
2023-03-09 16:21:25 -0800

    change import for netlify

--- src/pages/api/recipes/seed.ts
+++ src/pages/api/recipes/seed.ts
@@ -1,4 +1,4 @@
-import { dbConnect } from "@/utils/dbConnect";
+import dbConnect from "@/utils/dbConnect";
 import Recipe from "@/models/Recipe";
 import { NextApiRequest, NextApiResponse } from "next";
 import recipeSeed from "../../../data/recipes.json";
