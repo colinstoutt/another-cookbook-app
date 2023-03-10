9ace2cc6592aa0db084be8bcc8d4b4adf554737a
cps97 <colinstoutt@gmail.com>
2023-03-09 16:10:26 -0800

    add CI to build script

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  DEV: "https://localhost:3000/api/recipes/",
-  PROD: "https://next-js-ts-cookbook-knejvs4f2-colinstoutt.vercel.app/api/recipes/",
+  DEV: "http://localhost:3000/api/recipes/",
+  PROD: "http://next-js-ts-cookbook-knejvs4f2-colinstoutt.vercel.app/api/recipes/",
 };
 
 export default config;
