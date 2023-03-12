f2cab5453413a822d7f0b7bc1900bcba4b30d992
cps97 <colinstoutt@gmail.com>
2023-03-11 23:34:31 -0800

    add cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: [
+    {
+      source: "/api/recipes/:id*",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET, POST, PUT, DELETE",
+        },
+      ],
+    },
+  ],
   images: {
     remotePatterns: [
       {
@@ -9,24 +21,11 @@ const nextConfig = {
       },
     ],
   },
-  headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        destination: "http://localhost:3000/:path*", // Set this to the URL of your server
       },
     ];
   },
