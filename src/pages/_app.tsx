44e8e5f85d88902084fe92df51f0d36ea5df592b
cps97 <colinstoutt@gmail.com>
2023-03-11 17:43:27 -0800

    import new styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/scss/style.scss";
 import "@/scss/Navbar.scss";
 import "@/scss/RecipeCard.scss";
 import "@/scss/Index.scss";
+import "@/scss/New.scss";
+import "@/scss/[id].scss";
 
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
